3797e6d9eafbf403f0fd1bb17c348b2d2ec8055c
EmmaAbe <emma.abe.bradford@gmail.com>
2021-07-09 17:03:53 -0500

    created project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
+  state = {
+    todos: [
+      { id: 1, content:"watch untamed" },
+      { id: 2, content:"read mdzs" }
+    ]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
